2c17a6c4eb60d1847b9db015ae4ea65ba06a4b89
Sarthak Mishra <msarthak785@gmail.com>
2024-07-11 14:51:25 -0400

    made the form functional & table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,91 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fyle';
+  title = 'Fyle Workout';
+
+  userData = [
+    {
+      id: 1,
+      name: 'John Doe',
+      workouts: [
+        { type: 'Running', minutes: 30 },
+        { type: 'Cycling', minutes: 45 }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      workouts: [
+        { type: 'Swimming', minutes: 60 },
+        { type: 'Running', minutes: 20 }
+      ]
+    },
+    {
+      id: 3,
+      name: 'Mike Johnson',
+      workouts: [
+        { type: 'Yoga', minutes: 50 },
+        { type: 'Cycling', minutes: 40 }
+      ]
+    },
+  ];
+
+  name = '';
+  workoutType = '';
+  workoutMinutes: number | null = null;
+
+  workoutTypes = ['Cycling', 'Swimming', 'Yoga', 'Running'];
+
+  addWorkout() {
+    if (this.name && this.workoutType && this.workoutMinutes) {
+      const newWorkout = {
+        id: this.userData.length + 1,
+        name: this.name,
+        workouts: [
+          {
+            type: this.workoutType,
+            minutes: this.workoutMinutes
+          }
+        ]
+      };
+      this.userData.push(newWorkout);
+      this.saveToLocalStorage();
+      this.resetForm();
+    }
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('userData', JSON.stringify(this.userData));
+  }
+
+  resetLocalStorage() {
+    localStorage.clear();
+    this.userData = [];
+    this.name = '';
+    this.workoutType = '';
+    this.workoutMinutes = null;
+  }
+
+  resetForm() {
+    this.name = '';
+    this.workoutType = '';
+    this.workoutMinutes = null;
+  }
+
+  ngOnInit() {
+    const data = localStorage.getItem('userData');
+    if (data) {
+      this.userData = JSON.parse(data);
+    }
+  }
 }
